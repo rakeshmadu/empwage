c52de530f8ed71583f197041773eb01f339ba4ad
Madu Mahesh Kumar <rakeshmadu24@gmail.com>
2022-07-24 15:36:35 +0530

    uc5

--- src/main/java/com/bridgelabz/EmpWageBuilder.java
+++ src/main/java/com/bridgelabz/EmpWageBuilder.java
@@ -1,8 +1,9 @@
+
 package com.bridgelabz;
 
 public class EmpWageBuilder {
     public static void main(String[] args) {
-        System.out.println("Welcome to employee wage computation ");
+        System.out.println("Welcome to emp wage computation ");
 
         final int IS_FULL_TIME = 1;
         final int IS_PART_TIME = 2;
@@ -10,20 +11,29 @@ public class EmpWageBuilder {
         int FULL_TIME = 8;
         int PART_TIME = 4;
 
-        double attendance = Math.floor(Math.random() * 10) % 3;
-        switch ((int) attendance) {
-            case IS_FULL_TIME -> {
-                System.out.println("Employee wage = " + WAGE_PER_HR*FULL_TIME);
-                break;
+        int TOTAL_WORKING_DAYS = 20;
+
+        int wage_per_month = 0;
+        int emp_hr = 0;
+
+        for (int day = 0; day<TOTAL_WORKING_DAYS; day++) {
+            double attendance = Math.floor(Math.random() * 10) % 3;
+            switch ((int) attendance) {
+                case IS_FULL_TIME -> {
+                    emp_hr = FULL_TIME;
+                    break;
+                }
+                case IS_PART_TIME -> {
+                    emp_hr = PART_TIME;
+                    break;
+                }
+                default -> {
+                    emp_hr = 0;
+                    break;
+                }
             }
-            case IS_PART_TIME -> {
-                System.out.println("Employee wage = " + WAGE_PER_HR*PART_TIME);
-                break;
-            }
-            default -> {
-                System.out.println("Employee wage = " + 0);
-                break;
-            }
+            wage_per_month+= WAGE_PER_HR*emp_hr;
         }
+        System.out.println("Wage per month = "  + wage_per_month);
     }
 }
