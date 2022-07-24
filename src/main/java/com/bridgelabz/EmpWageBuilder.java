76f1d1c0eba52b45d577fcf51bbbef578bba3d49
Madu Mahesh Kumar <rakeshmadu24@gmail.com>
2022-07-24 15:20:36 +0530

    uc1

--- src/main/java/com/bridgelabz/EmpWageBuilder.java
+++ src/main/java/com/bridgelabz/EmpWageBuilder.java
@@ -2,11 +2,11 @@ package com.bridgelabz;
 
 public class EmpWageBuilder {
     public static void main(String[] args) {
-        System.out.println("Welcome to emp wage computation ");
+        System.out.println("Welcome to employee wage computation ");
 
         int IS_FULL_TIME = 1;
         int attendance = (int)Math.floor(Math.random() * 10) % 2;
-        if(attendance == IS_FULL_TIME) System.out.println("Emp is present");
-        else System.out.println("Emp is absent");
+        if(attendance == IS_FULL_TIME) System.out.println("Employee is present");
+        else System.out.println("Employee is absent");
     }
 }
