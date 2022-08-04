516088d1e3cc976691b060fe23eeab1ce304dfc7
Madu Mahesh Kumar <rakeshmadu24@gmail.com>
2022-08-04 15:49:37 +0530

    uc7

--- src/main/java/com/bridgelabz/EmpWageBuilder.java
+++ src/main/java/com/bridgelabz/EmpWageBuilder.java
@@ -1,45 +1,45 @@
 package com.bridgelabz;
 
 public class EmpWageBuilder {
-    public static void main(String[] args) {
-        System.out.println("Welcome to employee wage computation ");
-
-        final int IS_FULL_TIME = 1;
-        final int IS_PART_TIME = 2;
+    
+    public static void calculateTotalWage()
+    {
+        final int PART_TIME = 1;
+        final int FULL_TIME = 2;
         final int WAGE_PER_HR = 20;
-        final int FULL_TIME = 8;
-        final int PART_TIME = 4;
-
         final int MAX_WORKING_DAYS = 20;
-        final int MAX_HRS = 70;
+        final int MAX_WORKING_HRS = 100;
 
-        int wage_per_month = 0;
-        int total_working_hrs = 0;
-        int emp_hr = 0;
-        int day = 0;
+        int totalWage = 0;
+        int workingHrs = 0;
+        System.out.printf("%5s     %5s     %5s     %5s\n", "Day", "Workinghrs", "Wage", "Total working hrs");
+        for (int day = 1, totalWorkingHrs = 0; day <= MAX_WORKING_DAYS
+                && totalWorkingHrs < MAX_WORKING_HRS; day++, totalWorkingHrs += workingHrs)
+        {
 
-        while ( total_working_hrs < MAX_HRS && day < MAX_WORKING_DAYS) {
-            day++;
-            double attendance = Math.floor(Math.random() * 10) % 3;
-            switch ((int) attendance) {
-                case IS_FULL_TIME -> {
-                    emp_hr = FULL_TIME;
-                    break;
-                }
-                case IS_PART_TIME -> {
-                    emp_hr = PART_TIME;
-                    break;
-                }
-                default -> {
-                    emp_hr = 0;
-                    break;
-                }
+            int empType = (int) (Math.random() * 100) % 3;
+            switch (empType)
+            {
+            case FULL_TIME:
+                workingHrs = 8;
+                break;
+            case PART_TIME:
+                workingHrs = 4;
+                break;
+            default:
+                workingHrs = 0;
+                break;
             }
-            total_working_hrs+=emp_hr;
-            wage_per_month+= WAGE_PER_HR*emp_hr;
+            int wage = workingHrs * WAGE_PER_HR;
+            totalWage += wage;
+            System.out.printf("%5d       %5d      %5d      %5d\n", day, workingHrs, wage, totalWorkingHrs + workingHrs);
+
         }
-        System.out.println("No of days : "  + day);
-        System.out.println("No of hrs : "  + total_working_hrs);
-        System.out.println("Wage per month = "  + wage_per_month);
+        System.out.println("Total wage for a month is " + totalWage);
+    }
+
+    public static void main(String args[])
+    {
+        calculateTotalWage();
     }
 }
